b89337ae39282311e429db615b58205f18dd06dc
bswa006 <bswa006@gmail.com>
2025-06-19 21:45:19 +0530

    remove play demo button

--- app/workflow/components/WorkflowHeader.tsx
+++ app/workflow/components/WorkflowHeader.tsx
@@ -75,7 +75,8 @@ export function WorkflowHeader({
           </div>
         </div>
 
-        {/* Play Demo Controls */}
+        {/* Play Demo Controls - Commented out */}
+        {/* 
         <motion.button
           onClick={handlePlayClick}
           className="mercury-button flex items-center gap-2 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors focus:outline-none focus:ring-2 focus:ring-blue-500/20"
@@ -100,6 +101,7 @@ export function WorkflowHeader({
             </>
           )}
         </motion.button>
+        */}
       </div>
     </motion.div>
   );
